cc89a4b4e0f88a019b45977e1014bcdd681e0096
Gourab Sarkar <gourab.sarkar01@outlook.com>
2025-10-17 12:10:38 +0530

    added routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Contact } from './contact/contact';
+import { About } from './about/about';
+import { Privacy } from './privacy/privacy';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home,
+        pathMatch: 'full'
+    },
+    {
+        path: 'contact',
+        component: Contact
+    },
+    {
+        path: 'about',
+        component: About
+    },
+    {
+        path: 'privacy',
+        component: Privacy
+    }
+];
