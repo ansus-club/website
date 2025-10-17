54e5e388694e19520d2304c6bd719ab307410835
Gourab Sarkar <gourab.sarkar01@outlook.com>
2025-10-17 14:13:08 +0530

    updates for home component

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './home.css'
 })
 export class Home {
-
+  imageList: string[] = [
+    "images/1.png",
+    "images/2.png",
+    "images/3.png",
+    "images/4.png",
+    "images/5.png"
+  ]
 }
