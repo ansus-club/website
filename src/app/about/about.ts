955a458ce94d8821b5cfea189c4e93c3f5c1e622
Gourab Sarkar <gourab.sarkar01@outlook.com>
2025-12-30 23:46:12 +0530

    added dynamic content for about page

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { People } from '../services/people-service/people';
 
 @Component({
   selector: 'app-about',
@@ -7,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './about.css'
 })
 export class About {
+  peopleService = inject(People)
+  keyPeople: any
 
+  ngOnInit() {
+    this.peopleService.getKeyPeople().subscribe(res => {
+      this.keyPeople = res;
+    });
+  }
 }
