1aae12ef78712b1109980b488c4c74c370b32f79
Gourab Sarkar <gourab.sarkar01@outlook.com>
2026-01-03 15:00:27 +0530

    updated about component

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,5 +1,7 @@
-import { Component, inject } from '@angular/core';
-import { People } from '../services/people-service/people';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
+import { PeopleService } from '../services/people-service/people.service';
+import { KeyPeople, KeyPeopleResponse } from '../models/key-people.model';
+import { Members } from '../models/members.model';
 
 @Component({
   selector: 'app-about',
@@ -8,12 +10,46 @@ import { People } from '../services/people-service/people';
   styleUrl: './about.css'
 })
 export class About {
-  peopleService = inject(People)
-  keyPeople: any
+  private peopleService = inject(PeopleService)
+  private destroySub = inject(DestroyRef);
+  isKpLoading = signal(false);
+  isMemLoading = signal(false);
+  kpError = signal("");
+  mError = signal("");
+  keyPeople = signal<KeyPeople[] | undefined>(undefined);
+  members = signal<Members | undefined>(undefined);
 
   ngOnInit() {
-    this.peopleService.getKeyPeople().subscribe(res => {
-      this.keyPeople = res;
+    this.isKpLoading.set(true);
+    this.isMemLoading.set(true);
+
+    const kpSub = this.peopleService.getKeyPeople().subscribe({
+      next: (kpr: KeyPeopleResponse) => {
+        this.keyPeople.set(kpr.data);
+      },
+      error: (err: Error) => {
+        this.kpError.set(err.message);
+      },
+      complete: () => {
+        this.isKpLoading.set(false);
+      },
+    });
+
+    const mSub = this.peopleService.getMembers().subscribe({
+      next: (members: Members) => {
+        this.members.set(members);
+      },
+      error: (err: Error) => {
+        this.mError.set(err.message);
+      },
+      complete: () => {
+        this.isMemLoading.set(false);
+      }
+    })
+
+    this.destroySub.onDestroy(() => {
+      kpSub.unsubscribe();
+      mSub.unsubscribe();
     });
   }
 }
