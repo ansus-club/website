692789f8e517c58cfcd183af36635e806f32d393
Gourab Sarkar <gourab.sarkar01@outlook.com>
2026-01-03 15:00:08 +0530

    added people service

---
+++ src/app/services/people-service/people.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { KeyPeopleResponse } from '../../models/key-people.model';
+import { catchError, map, throwError } from 'rxjs';
+import { Members } from '../../models/members.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PeopleService {
+  private httpClient = inject(HttpClient);
+
+  getKeyPeople() {
+    return this.httpClient.get<KeyPeopleResponse>('../../data/key-people.json').pipe(
+      catchError((err) => {
+        console.log(err);
+        return throwError(() => new Error("Something went wrong while fetching key people. Please, try again later."));
+      })
+    );
+  }
+
+  getMembers() {
+    return this.httpClient.get<Members>('../../data/members.json').pipe(
+      // sort the members alphabetically ignoring case
+      map((members) => ({
+        ...members,
+        data: [...members.data].sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' }))
+      })),
+      catchError((err) => {
+        console.log(err);
+        return throwError(() => new Error("Something went wrong while fetching our members. Please, try again later."));
+      })
+    );
+  }
+
+}
