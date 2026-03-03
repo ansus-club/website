01cc048865c9c44d8b5be52d3397089f9cbeb7f8
ansus-club <anandanagar.sus@gmail.com>
2026-03-03 12:47:41 +0000

    updates for home

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { KeyPeople, KeyPeopleResponse } from '../models/key-people.model';
+import { PeopleService } from '../services/people-service/people.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+  private peopleService = inject(PeopleService)
+  private destroySub = inject(DestroyRef);
+
+  isKpLoading = signal(false);
+  kpError = signal("");
+  keyPeople = signal<KeyPeople[] | undefined>(undefined);
+
+  ngOnInit() {
+    this.isKpLoading.set(true);
+
+    const kpSub = this.peopleService.getKeyPeople().subscribe({
+      next: (kpr: KeyPeopleResponse) => {
+        this.keyPeople.set(kpr.data);
+      },
+      error: (err: Error) => {
+        this.kpError.set(err.message);
+      },
+      complete: () => {
+        this.isKpLoading.set(false);
+      },
+    });
+
+    this.destroySub.onDestroy(() => {
+      kpSub.unsubscribe();
+    });
+  }
 }
